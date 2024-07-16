aa15e3117fe62155cc561f6fdd36d99e82e5052c
Nigel Saurino <nigelsaurino@gmail.com>
2024-07-16 11:53:09 -0400

    Starting HW9 ex2

---
+++ ex2/ex2.cpp
@@ -0,0 +1,11 @@
+
+/*
+- Write a simple UDP client-server application.
+- The server should listen on a specified port and echo back any message it receives.
+- The client should send a message to the server and print the response.
+*/
+
+int Ex2(){
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #pragma once
 #include <stdio.h>
 #include "ex1/ex1.cpp"
+#include "ex2/ex2.cpp"
 
 int main(int, char**){
-    Ex1();
+    // Ex1();
+    Ex2();
 }
